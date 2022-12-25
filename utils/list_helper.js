8d82f15262ac6e4b498efc467bfbfeaea1df8ae2
Staffan Småros <staffan.smaros@gmail.com>
2022-12-25 14:33:11 +0200

    Completed exercise 4.4 and fixed linting

---
+++ utils/list_helper.js
@@ -0,0 +1,16 @@
+const dummy = (blogs) => {
+  return blogs ? 1 : 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.reduce( reducer, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
