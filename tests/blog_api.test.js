b131d48d6fec9ce849eabe315692b0641c1e7a3c
Staffan Småros <staffan.smaros@gmail.com>
2023-02-12 09:22:58 +0200

    Completed exercise 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -109,6 +109,19 @@ test('a blog without title or url is not added', async () => {
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
+test('deleting a blog by id', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete('/api/blogs/' + blogToDelete.id)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
